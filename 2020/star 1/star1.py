0d7e6274da3ffdf641b3a9236ea4f9da36383181
julien-soulier <julien.soulier@mail.com>
2020-12-27 15:14:53 +0100

    cleanup code for star1.py

--- 2020/star 1/star1.py
+++ 2020/star 1/star1.py
@@ -1,18 +1,36 @@
 f = open("input.txt", "r")
-a_str = f.readlines()
-a_nbr = []
-for s in a_str:
-    a_nbr.append(int(s))
+lines = f.read().split('\n')
+lines.remove('')
+f.close()
 
-for n in a_nbr:
-    for m in a_nbr:
+a_nbr = list(map(int, lines))
+
+def part1():
+    for n in a_nbr:
         try:
-            i = a_nbr.index(2020-n-m)
+            i = a_nbr.index(2020-n)
         except:
             pass
         else:
             l=a_nbr[i]
-            print("{}+{}+{}={} and {}*{}*{}={}".format(n,m,l,n+m+l,n,m,l,n*m*l))
-            break
+            print("{}+{}={} and {}*{}={}".format(n,l,n+l,n,l,n*l))
+            return n*l
 
-f.close()
+def part2():
+    for n in a_nbr:
+        for m in a_nbr:
+            try:
+                i = a_nbr.index(2020-n-m)
+            except:
+                pass
+            else:
+                l=a_nbr[i]
+                print("{}+{}+{}={} and {}*{}*{}={}".format(n,m,l,n+m+l,n,m,l,n*m*l))
+                return n*m*l
+
+
+res1 = part1()
+print("Soution part1 is {}".format(res1))
+
+res2 = part2()
+print("Soution part2 is {}".format(res2))
