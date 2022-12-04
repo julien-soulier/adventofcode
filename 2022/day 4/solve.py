87a8ec4909491588d7c08ee772162c54cdeacf2c
julien-soulier <julien.soulier@mail.com>
2022-12-04 09:58:13 +0100

    day 4

---
+++ 2022/day 4/solve.py
@@ -0,0 +1,16 @@
+#/bin/env python3
+
+from parse import *
+
+def overlap(pair):
+    r = parse("{:d}-{:d},{:d}-{:d}", pair)
+    full = 1 if (r[0]>=r[2] and r[1]<=r[3]) or (r[0]<=r[2] and r[1]>=r[3]) else 0
+    part = 1 if (r[0]>=r[2] and r[0]<=r[3]) or (r[1]>=r[2] and r[1]<=r[3]) or (r[2]>=r[0] and r[2]<=r[1]) or (r[3]>=r[0] and r[3]<=r[1]) else 0
+    return (full, part)
+
+
+with open("input.txt", "r") as f:
+    res = [overlap(pair) for pair in f.read().splitlines()]
+    print(f"Soution part1 is {sum(map(lambda x: x[0], res))}")
+    print(f"Soution part2 is {sum(map(lambda x: x[1], res))}")
+
