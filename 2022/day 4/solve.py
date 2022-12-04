d079d461189bf6433ff0afa16d7e119e888352e8
julien-soulier <julien.soulier@mail.com>
2022-12-04 13:52:41 +0100

    use readlines()

--- 2022/day 4/solve.py
+++ 2022/day 4/solve.py
@@ -5,13 +5,13 @@ import functools
 
 #### CODING STYLE 1 #####
 def overlap(line):
-    r = parse("{:d}-{:d},{:d}-{:d}", line)
+    r = parse("{:d}-{:d},{:d}-{:d}\n", line)
     full = 1 if (r[0]>=r[2] and r[1]<=r[3]) or (r[0]<=r[2] and r[1]>=r[3]) else 0
     part = 1 if (r[0]>=r[2] and r[0]<=r[3]) or (r[1]>=r[2] and r[1]<=r[3]) or (r[2]>=r[0] and r[2]<=r[1]) or (r[3]>=r[0] and r[3]<=r[1]) else 0
     return (full, part)
 
 with open("input.txt", "r") as f:
-    res = functools.reduce(lambda x,y: (y[0]+x[0], y[1]+x[1]), (overlap(line) for line in f.read().splitlines()))   
+    res = functools.reduce(lambda x,y: (y[0]+x[0], y[1]+x[1]), (overlap(line) for line in f.readlines()))   
     print(f"Soution part1 is {res[0]}")
     print(f"Soution part2 is {res[1]}")
 
@@ -27,8 +27,8 @@ def overlap_part(r):
 
 with open("input.txt", "r") as f:
     sum_f = sum_p = 0    
-    for line in f.read().splitlines():
-        pair = parse("{:d}-{:d},{:d}-{:d}", line)
+    for line in f.readlines():
+        pair = parse("{:d}-{:d},{:d}-{:d}\n", line)
 
         sum_f += overlap_full(pair)
         sum_p += overlap_part(pair)
