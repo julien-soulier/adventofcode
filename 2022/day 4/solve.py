8e5dcaa81417124e8df7edea59ed4ecf896ee6f9
julien-soulier <julien.soulier@mail.com>
2022-12-04 10:58:42 +0100

    another version with single loop parsing

--- 2022/day 4/solve.py
+++ 2022/day 4/solve.py
@@ -2,6 +2,7 @@
 
 from parse import *
 
+#### CODING STYLE 1 #####
 def overlap(pair):
     r = parse("{:d}-{:d},{:d}-{:d}", pair)
     full = 1 if (r[0]>=r[2] and r[1]<=r[3]) or (r[0]<=r[2] and r[1]>=r[3]) else 0
@@ -13,4 +14,26 @@ with open("input.txt", "r") as f:
     res = [overlap(pair) for pair in f.read().splitlines()]
     print(f"Soution part1 is {sum(x[0] for x in res)}")
     print(f"Soution part2 is {sum(x[1] for x in res)}")
+
+
+#### CODING STYLE 2 #####
+print(f"===========================")
+
+def overlap_full(r):
+    return 1 if (r[0]>=r[2] and r[1]<=r[3]) or (r[0]<=r[2] and r[1]>=r[3]) else 0
+
+def overlap_part(r):
+    return 1 if (r[0]>=r[2] and r[0]<=r[3]) or (r[1]>=r[2] and r[1]<=r[3]) or (r[2]>=r[0] and r[2]<=r[1]) or (r[3]>=r[0] and r[3]<=r[1]) else 0
+
+with open("input.txt", "r") as f:
+    sum_f = sum_p = 0    
+    for line in f.read().splitlines():
+        pair = parse("{:d}-{:d},{:d}-{:d}", line)
+
+        sum_f += overlap_full(pair)
+        sum_p += overlap_part(pair)
+
+    print(f"Soution part1 is {sum_f}")
+    print(f"Soution part2 is {sum_p}")
+    
 
