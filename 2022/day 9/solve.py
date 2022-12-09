ce8caea0960eac89f6172021afad5b45d40c23c0
julien-soulier <julien.soulier@mail.com>
2022-12-09 13:40:41 +0100

    day 9: use coordinates as tuple

--- 2022/day 9/solve.py
+++ 2022/day 9/solve.py
@@ -2,24 +2,28 @@
 
 from parse import *
 
-def sign(v):
-    return 1 if v>0 else -1 if v<0 else 0
+def tuple_sign(a):
+    return tuple(1 if v>0 else -1 if v<0 else 0 for v in a)
 
-def update_knot(Hx, Hy, Tx, Ty):
-    dx = Hx - Tx
-    dy = Hy - Ty
+def tuple_add(a, b):
+    return tuple(i + j for i, j in zip(a,b))
+
+def tuple_neg(a):
+    return tuple(-i for i in a)
+
+def update_knot(H, T):
+    D = tuple_add(H, tuple_neg(T))
     
-    if abs(dx)<2 and abs(dy)<2:
-        return Tx, Ty
+    if all(abs(i)<2 for i in D):
+        return T
 
-    return Tx + sign(dx), Ty + sign(dy)
+    return tuple_add(T, tuple_sign(D))
 
 MVT = {'U': (0,1), 'D': (0,-1), 'R': (1,0), 'L': (-1,0)}
 
 def simulate(knots=2):
     visited = {(0,0)}
-    Kx = [0] * knots
-    Ky = [0] * knots
+    K = [(0,0)] * knots
     with open("input.txt", "r") as f:
         for line in f.readlines():
             r = parse("{cmd} {count:d}", line.strip())
@@ -27,14 +31,13 @@ def simulate(knots=2):
                 continue
             
             for _ in range(r['count']):
-                Kx[0] += MVT[r['cmd']][0]
-                Ky[0] += MVT[r['cmd']][1]
+                K[0] = tuple_add(K[0], MVT[r['cmd']])
                 
                 for k in range(1,knots):
-                    Kx[k], Ky[k] = update_knot(Kx[k-1], Ky[k-1], Kx[k], Ky[k])
+                    K[k] = update_knot(K[k-1], K[k])
                 
-                visited.add((Kx[-1], Ky[-1]))
-    return visited
+                visited.add(K[-1])
+    return len(visited)
 
-print(f"Soution part1 is {len(simulate())}")
-print(f"Soution part2 is {len(simulate(10))}")
+print(f"Soution part1 is {simulate(2)}")
+print(f"Soution part2 is {simulate(10)}")
